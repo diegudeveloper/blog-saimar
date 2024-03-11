9a919565dc4cc8970eab4d4a40dd2a9b57a272ff
Diegu <diegudeveloper@gmail.com>
2024-03-11 19:33:04 +0100

    Iniciando componentes de post y sidebar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,16 @@
-import Header from "../../header/Header"
-import "./home.css"
+import Header from "../../header/Header";
+import Post from "../../post/Post";
+import Sidebar from "../../sidebar/Sidebar";
+import "./home.css";
 
 export default function Home() {
   return (
-    <div className="home">
-        <Header/>
-        homepage
-    </div>
+    <>
+      <Header/>
+      <div className="home">
+        <Post />
+        <Sidebar />
+      </div>
+    </>
   )
 }

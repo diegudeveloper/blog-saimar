88fbfe55fd370dfcf1ca8400b7e72e11ca3c8041
Diegu <diegudeveloper@gmail.com>
2024-03-11 21:04:06 +0100

    configuacion del sidebar y la imagen

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -3,7 +3,17 @@ import "./sidebar.css"
 export default function Sidebar() {
   return (
     <div className="sidebar">
-        Sidebar
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src="https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt="" className="imageAbout"
+        />
+        <p>
+          Laboris sunt aute cupidatat velit magna velit ullamco dolore mollit
+          amet ex esse.Sunt eu ut nostrud id quis proident.
+        </p>
+      </div>
     </div>
   )
 }

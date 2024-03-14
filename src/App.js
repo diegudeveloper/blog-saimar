28936f866ce814c0424fe0496fdf78e2d75a2ff2
Diegu <diegudeveloper@gmail.com>
2024-03-14 18:14:20 +0100

    inicio de la pagina write y sus estilos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 function App() {
   return (
     <>
       <TopBar/>
-      <Single/>
+      <Write/>
     </>
   );
 }

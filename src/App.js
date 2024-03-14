b3285233d3378337dccdc7bf428fbdd5b65efe3d
Diegu <diegudeveloper@gmail.com>
2024-03-14 19:46:12 +0100

    Configuracion del pages register y sus estilos.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import TopBar from "./components/topbar/TopBar";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
@@ -10,7 +11,7 @@ function App() {
   return (
     <>
       <TopBar/>
-      <Login/>
+      <Register/>
     </>
   );
 }

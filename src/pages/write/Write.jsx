a136c9d73e27734aaaf204d31cda834e736b9f0d
Diegu <diegudeveloper@gmail.com>
2024-03-14 18:26:51 +0100

    culminacion de la configuracion y estilos del page write

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -1,9 +1,38 @@
-import "./write.css"
+import "./write.css";
 
 export default function Write() {
   return (
-    <div className='write'>
-        Write
+    <div className="write">
+      <img
+        className="writeImg"
+        src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+      <form className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i className="writeIcon ri-add-line"></i>
+          </label>
+          <input id="fileInput" type="file" style={{ display: "none" }} />
+          <input
+            className="writeInput"
+            placeholder="Title"
+            type="text"
+            autoFocus={true}
+          />
+        </div>
+        <div className="writeFormGroup">
+          <textarea
+            className="writeInput writeText"
+            placeholder="Tell your story..."
+            type="text"
+            autoFocus={true}
+          />
+        </div>
+        <button className="writeSubmit" type="submit">
+          Publish
+        </button>
+      </form>
     </div>
-  )
+  );
 }
